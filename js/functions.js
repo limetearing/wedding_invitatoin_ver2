4d6ad1fd30f5b89a3948cc5f323dc0c51faf9859
limetearing <kysuffer@gmail.com>
2024-07-29 21:53:07 +0900

    asdw

--- js/functions.js
+++ js/functions.js
@@ -1,28 +1,8 @@
-function copyToClipboard(link, message) {
-    // 임시 텍스트 영역 생성
-    const tempTextArea = document.createElement('textarea');
-    tempTextArea.value = link;
-
-    // 요소를 화면 밖에 위치시키기
-    tempTextArea.style.position = 'absolute';
-    tempTextArea.style.left = '-9999px';
-
-    document.body.appendChild(tempTextArea);
-    tempTextArea.select();
-    tempTextArea.setSelectionRange(0, 99999); // 모바일 장치 호환성
-
-    try {
-        const successful = document.execCommand('copy');
-        if (successful) {
-            alert(message);
-        } else {
-            alert('복사에 실패했습니다.');
-        }
-    } catch (err) {
-        alert('복사 중 오류가 발생했습니다.');
-        console.error('복사 중 오류:', err);
-    }
-
-    // 임시 텍스트 영역 삭제
-    document.body.removeChild(tempTextArea);
+function showAccountNumber() {
+    var newWindow = window.open("", "_blank", "width=300,height=200");
+    newWindow.document.write("<html><head><title>계좌번호</title></head><body>");
+    newWindow.document.write("<h2>계좌번호</h2>");
+    newWindow.document.write("<p>우리은행 123-456-7890</p>");
+    newWindow.document.write("<button onclick='window.close();'>닫기</button>");
+    newWindow.document.write("</body></html>");
 }
